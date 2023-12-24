0aebe8721615fafd239eae2c0d3105326c143cee
Raihan Abdillah <raihanabdillahlubis@gmail.com>
2023-12-24 17:22:04 +0700

    Front End Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
@@ -8,8 +8,12 @@ module.exports = {
         'main': "'Jost', sans-serif",
       },
       colors: {
-        'main': '#241b35',
-        'secondary': '#342a45',
+        'main': '#0f0f11',
+        'container': '#17191c',
+        'input': '#16181b',
+        'border': 'rgba(165,170,180, 0.2)',
+        'text': '#a5aab4',
+        'blue': '#18b8ee',
       }
     },
   },
